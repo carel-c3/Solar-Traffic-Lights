f6815b60893883245834a010635a659969c16cb4
carel-c3 <72303909+carel-c3@users.noreply.github.com>
2020-10-03 14:08:42 +0200

    Add files via upload

---
+++ traffic_lights_event_test1_MEC.ino
@@ -0,0 +1,56 @@
+//Comms test - MEC Code
+
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin(); // join i2c bus (address optional for master)
+}
+
+byte x = 0;
+
+void loop()
+{
+  //Event1
+  x = 1;
+  Wire.beginTransmission(4); // broadcast to device #4
+  Wire.write(x);              // sends one byte  
+  Wire.endTransmission();    // stop transmitting
+  delay(5000);
+  
+  //Event2
+  x = 2;
+  Wire.beginTransmission(4); // broadcast to device #4
+  Wire.write(x);              // sends one byte  
+  Wire.endTransmission();    // stop transmitting
+  delay(2000);
+  
+  //Event3
+  x = 3;
+  Wire.beginTransmission(4); // broadcast to device #4
+  Wire.write(x);              // sends one byte  
+  Wire.endTransmission();    // stop transmitting
+  delay(1000);
+  
+  //Event4
+  x = 4;
+  Wire.beginTransmission(4); // broadcast to device #4
+  Wire.write(x);              // sends one byte  
+  Wire.endTransmission();    // stop transmitting
+  delay(5000);
+  
+  //Event5
+  x = 5;
+  Wire.beginTransmission(4); // broadcast to device #4
+  Wire.write(x);              // sends one byte  
+  Wire.endTransmission();    // stop transmitting
+  delay(2000);
+  
+  //Event6
+  x = 6;
+  Wire.beginTransmission(4); // broadcast to device #4
+  Wire.write(x);              // sends one byte  
+  Wire.endTransmission();    // stop transmitting
+  delay(1000);
+  
+}
---
+++ traffic_lights_event_test2_SLC1.ino
@@ -0,0 +1,83 @@
+//Comms Test - SLC Code
+
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin(4);                // join i2c bus with address #4
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
+  
+  pinMode(13, OUTPUT);//Setup Red LED
+  pinMode(12, OUTPUT);//Setup Yellow LED
+  pinMode(11, OUTPUT);//Setup Greeg LED
+  
+}
+
+void loop()
+{
+  delay(100);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+
+void receiveEvent(int howMany)
+{
+
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
+  
+  
+  switch (x) {
+  case 1:
+    //Control Lights for Event1
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, HIGH);
+    break;
+    
+  case 2:
+    //Control Lights for Event2
+    digitalWrite(13, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+    break;
+    
+   case 3:
+    //Control Lights for Event3
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    break; 
+   
+    case 4:
+    //Control Lights for Event4
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    break;
+    
+  	case 5:
+    //Control Lights for Event5
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    break; 
+    
+   case 6:
+    //Control Lights for Event6
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    break; 
+       
+  default:
+    // if nothing else matches, do the default
+    // default is optional
+    break;
+}
+  
+  
+  
+}
