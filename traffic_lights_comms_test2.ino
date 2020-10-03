7551c78180f9301fbe99f90628fc451f74071dc5
carel-c3 <72303909+carel-c3@users.noreply.github.com>
2020-10-03 14:13:11 +0200

    Add files via upload

---
+++ traffic_lights_comms_test2.ino
@@ -0,0 +1,25 @@
+//Comms Test - SLC Code
+
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin(4);                // join i2c bus with address #4
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
+}
+
+void loop()
+{
+  delay(100);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+
+void receiveEvent(int howMany)
+{
+
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
+}
